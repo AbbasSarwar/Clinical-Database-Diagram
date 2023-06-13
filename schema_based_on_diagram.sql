a43e5e484b79fbc5d0d37fabbfcd2c29af467669
Seyi Oyemade <seyioyemade918@gmail.com>
2023-06-13 15:43:47 +0100

    Create invoice_items and invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -27,3 +27,28 @@ Create TABLE treatments (
 ALTER TABLE treatments 
 ADD FOREIGN KEY (id)
 REFERENCES medical_histories (id) 
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price decimal,
+    quantity int,
+    total_price decimal,
+    invoice_id int,
+    treatment_id int,
+    FOREIGN KEY (treatment_id)
+    REFERENCES treatments (id)
+)
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id int,
+    FOREIGN KEY (medical_history_id)
+    REFERENCES medical_histories (id)
+)
+
+ALTER TABLE invoice_items
+ADD FOREIGN KEY (invoice_id)
+REFERENCES invoices (id)
